060ee19b420bfd6437760d33043ab0978bb6a00b
Jefferson Oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-05-06 22:18:42 -0300

    Adicionar arquivos de projeto.

---
+++ gestor-de-pacientes/Class/Paciente.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace gestor_de_pacientes.Class
+{
+    class Paciente
+    {
+        static int nextID;
+        private int idPaciente;
+        private string nome;
+        private int idade;
+        private string risco;
+        private string telefone;
+        private string email;
+        private char preferencial;
+
+        public Paciente() 
+        { 
+            this.idPaciente = Interlocked.Increment(ref nextID);
+        }
+
+        public Paciente(string nome)
+        {
+            this.nome = nome;
+            this.idPaciente = Interlocked.Increment(ref nextID);
+        }
+
+        public Paciente(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.idPaciente = Interlocked.Increment(ref nextID);
+        }
+
+        public Paciente(string nome, int idade, string risco)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.risco = risco;
+            this.idPaciente = Interlocked.Increment(ref nextID);
+        }
+
+        public Paciente(string nome, int idade, string risco, string telefone)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.risco = risco;
+            this.telefone = telefone;
+            this.idPaciente = Interlocked.Increment(ref nextID);
+        }
+
+        public Paciente(string nome, int idade, string risco, string telefone, string email)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.risco = risco;
+            this.telefone = telefone;
+            this.email = email;
+            this.idPaciente = Interlocked.Increment(ref nextID);
+        }
+
+        public Paciente(string nome, int idade, string risco, string telefone, string email, char preferencial)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.risco = risco;
+            this.telefone = telefone;
+            this.email = email;
+            this.preferencial = preferencial;
+            this.idPaciente = Interlocked.Increment(ref nextID);
+        }
+
+        public int IdPaciente
+        {
+            get
+            {
+                return this.idPaciente;
+            }
+
+            set
+            {
+                this.idPaciente = value;
+            }
+        }
+        public string Nome
+        { 
+            get 
+            { 
+                return this.nome; 
+            } 
+            set 
+            {  
+                this.nome = value; 
+            }
+        }
+
+        public int Idade
+        {
+            get 
+            {
+                return this.idade;
+            }
+
+            set 
+            {
+                this.idade = value;
+            }
+        }
+
+        public string Risco
+        {
+            get
+            {
+                return this.risco;
+            }
+
+            set
+            {
+                risco = value;
+            }
+        }
+
+        public string Telefone
+        {
+            get
+            {
+                return this.telefone;
+            }
+
+            set
+            {
+                this.telefone = value;
+            }
+        }
+
+        public string Email
+        {
+            get 
+            {
+                return this.email;
+            }
+
+            set
+            {
+                this.email = value;
+            }
+        }
+
+        public char Preferencial
+        {
+            get
+            {
+                return this.preferencial;
+            }
+
+            set
+            {
+                
+                while (this.preferencial != 'S' && this.preferencial != 'N')
+                {
+                    Console.WriteLine("Digite novamente S ou N");
+                    this.preferencial = char.Parse(Console.ReadLine().ToUpper());
+                }
+            }
+        }
+
+        public void mostrarDados()
+        {
+            Console.WriteLine($"Nome: {this.nome}");
+            Console.WriteLine($"Idade: {this.idade}");
+            Console.WriteLine($"Risco: {this.risco}");
+            Console.WriteLine($"Telefone: {this.telefone}");
+            Console.WriteLine($"Email: {this.email}");
+            
+        }
+
+    }
+}
---
+++ gestor-de-pacientes/Program.cs
@@ -0,0 +1,98 @@
+﻿using gestor_de_pacientes.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestor_de_pacientes
+{
+    class Cadastro 
+    {
+        static void Main(string[] args)
+        {
+            Paciente[] pacientes = new Paciente[10];
+            char opcao = 'a';
+
+            string nome;
+            int idade;
+            string risco;
+            string telefone;
+            string email;
+            char preferencial;
+            
+
+            int posicaoArray = 0;
+
+                Console.WriteLine("Gestor de fila de pacientes");
+            do
+            {
+                Console.WriteLine("Digite uma opção!");
+                Console.WriteLine("1 --- Cadastrar um paciente");
+                Console.WriteLine("2 --- Excluir um paciente");
+                Console.WriteLine("3 --- Listar a fila de prioridade");
+                Console.WriteLine("4 --- Atender um paciente");
+
+                opcao = char.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case '1':
+                        Paciente paciente = new Paciente();
+
+                        Console.WriteLine("Nome:");
+                        nome = Console.ReadLine();
+
+                        Console.WriteLine("Idade:");
+                        idade = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Risco:");
+                        risco = Console.ReadLine();
+
+                        Console.WriteLine("Telefone:");
+                        telefone = Console.ReadLine();
+
+                        Console.WriteLine("email:");
+                        email = Console.ReadLine();
+
+                        Console.WriteLine("Esse paciente é preferencial?");
+                        Console.WriteLine("Digite S ou N");
+                        preferencial = char.Parse(Console.ReadLine().ToUpper());
+
+                        paciente.Preferencial = preferencial;
+
+                        //Paciente paciente = new Paciente(nome, idade, risco, telefone, email, preferencial);
+                        pacientes[posicaoArray] = paciente;
+                        break;
+
+                    case '2':
+                        pacientes[posicaoArray].mostrarDados();
+                        break;
+
+                    case '3':
+                        for (int i = 0; i < pacientes.Length; i++)
+                        {
+                            Console.Write("ID do Paciente: " + pacientes[i].IdPaciente + " ");
+                            pacientes[i].mostrarDados();
+                            Console.WriteLine("--------------------------------------");
+                            if (pacientes[i] == null)
+                            {
+
+                            }
+                        }
+                        break;
+                }
+
+                posicaoArray++;
+
+            } while (opcao != 'q');
+
+            pacientes[0].mostrarDados();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
